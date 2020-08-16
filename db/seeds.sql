7136bd16a162de8097fdba2553d1d67f83a78112
Dilshan <dopatha@nc.rr.com>
2020-08-16 17:15:12 -0400

    updating files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetable Burger', FALSE);
+INSERT INTO burger (name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burger (name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burger (name, devoured) VALUES ('Vegetable Burger', FALSE);
