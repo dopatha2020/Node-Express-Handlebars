6a4794666d9da156addeb1209f36022f71fef131
Dilshan <dopatha@nc.rr.com>
2020-08-16 17:27:40 -0400

    updating

---
+++ public/js/burger.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".devour-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var burger_id = $(this).children(".burger_id").val();
+      $.ajax({
+        method: "PUT",
+        url: "/burger/" + burger_id
+      }).then(function(data) {
+        location.reload();
+      });
+    });
+});
+
